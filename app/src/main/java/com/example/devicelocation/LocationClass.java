1d87db403cc6af22e7ab11d90d42f0db99e40191
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-05-28 00:43:30 +0500

    1.2

--- app/src/main/java/com/example/devicelocation/LocationClass.java
+++ app/src/main/java/com/example/devicelocation/LocationClass.java
@@ -1,4 +1,90 @@
 package com.example.devicelocation;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.IntentSender;
+import android.location.Location;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.text.DateFormat;
+import java.util.Date;
+
 public class LocationClass {
+
+    // location updates interval - 10sec
+    // fastest updates interval - 5 sec
+    // location updates will be received if another app is requesting the locations
+    // than your app can handle
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = 5000;
+    private static final int REQUEST_CHECK_SETTINGS = 100;
+    private static final String TAG = "LocationClass";
+
+    private Context context;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private SettingsClient mSettingsClient;
+    private LocationRequest mLocationRequest;
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private LocationCallback mLocationCallback;
+    private Location mCurrentLocation;
+
+    private String mLastUpdateTime;
+
+    public LocationClass(Context context)
+    {
+        this.context = context;
+
+    }
+
+    private void init()
+    {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+        mSettingsClient = LocationServices.getSettingsClient(context);
+        mLocationCallback = new LocationCallback()
+        {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                mCurrentLocation = locationResult.getLastLocation();
+                mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+
+               // updateLocationUI();
+
+                mLocationRequest = new LocationRequest();
+                mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+                mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+                mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+                LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+                builder.addLocationRequest(mLocationRequest);
+                mLocationSettingsRequest = builder.build();
+
+            }
+        };
+
+    }
+
+  
+
+
+
 }
